d662011505fcf8c62560395e8a361c15a28be24f
Moises Arrona <57106162+moisesarrona@users.noreply.github.com>
2023-05-11 23:32:26 -0600

    Change javax to Jakatar

--- src/main/java/com/ed/store/entity/Category.java
+++ src/main/java/com/ed/store/entity/Category.java
@@ -1,18 +1,23 @@
 package com.ed.store.entity;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-@Entity(name= "categoryEntity")
-@Data @AllArgsConstructor @NoArgsConstructor @Builder
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
 @Table(name="tbl_categories")
 public class Category {
 	@Id
